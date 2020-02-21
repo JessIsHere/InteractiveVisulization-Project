98e3d33ff79364faa4f76e0f6579ded5500bfe69
jessica kim <J_Kim@users.noreply.github.com>
2020-02-20 19:49:09 -0600

    html update

--- Resources/js/logic.js
+++ Resources/js/logic.js
@@ -1,1 +1,17 @@
-APIKEY = ''
+d3.json(url).then(function(data){
+    console.log(data)
+});
+
+// var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/ACAData_DB';
+
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.collection("ACAStateData").findOne({}, function(err, result) {
+//       if (err) throw err;
+//       console.log(result.name);
+//       db.close();
+//     });
+//   });
