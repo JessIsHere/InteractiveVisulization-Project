b703325f85785b52df267f37b84c41b0f0f13eaa
jessica kim <J_Kim@users.noreply.github.com>
2020-02-22 13:37:56 -0600

    choropleth base format

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,48 @@
 var url = "/API/Data"
 
 var ACAData = d3.json(url).then(function(data){
-    console.log(data);
+    // console.log(data);
 });
+
+// Creating map object
+
+var myMap = L.map('map', {
+    center: [39.8283, - 98.5795],
+    zoom: 8 
+})
+
+// Tile Layer
+L.tileLayer(
+    'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+  {
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: API_KEY
+  }
+).addTo(myMap);
+
+var acaJson;
+
+// Load in Data
+d3.json(url, function (grabData) {
+    // Create choropleth layer
+    acaJson = L.choropleth(data, {
+        //  Define property to call
+        valueProperty: 'State',
+        // Set color scale
+        scale: ['#ffffb2', '#b10026'],
+
+        // Number of breaks in step range: Ammount of color saturation in gradient to move from one scale to another.
+        steps: 5,
+
+        // How am I defining my steps? Quartile - Health rating 
+        mode: 'q',
+        style: {
+            color: '#fff',
+            weight: 1,
+            fillOpacity: 0.8
+
+        }
+
+    })
+})
