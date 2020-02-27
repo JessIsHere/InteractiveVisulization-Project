035283707310d30f65a01729a87d651f9f04d375
jessica kim <J_Kim@users.noreply.github.com>
2020-02-27 12:06:01 -0600

    removed unecessary files

--- app.py
+++ app.py
@@ -45,32 +45,6 @@ def MapData():
     output.append(d)
     return jsonify(output)
 
-# @app.route("/API/Data/Map")
-# def map_data():
-#     aca_data = list(aca_state_data.find({}, {"_id": 0}))
-            
-#     # list comprehension
-#     # enrollment = [row["Data"]["Total_Enrollment"] if row["Data"]["Total_Enrollment"] >= -99999999999 else 0 for row in aca_data]
-#     rank = [row["Data"]["All_Determinants_Rank"] for row in aca_data]
-
-#     # state_abbr = [row["Data"]['State_Abbr'] for row in aca_data]
-#     state_name = [row["Data"]['State'] for row in aca_data]
-#     data_mapped = [{
-#         "type": "choropleth",
-#         "locationmode": "USA-states",
-#         "locations": state_name,
-#         'z': rank,
-#         # Change state name later to show associated value
-#         "text": state_name,
-#         "hoverinfo": "text",
-#         'zmin': 0,
-#         "zmax": max(rank),
-#     }]
-
-#     # sliding scale over years change color
-#     # on click load state chart
-#     return jsonify(data_mapped)
-
 
 @app.route("/About")
 def about():
--- static/js/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-const API_KEY ='pk.eyJ1IjoiamVzc2ljYXJucyIsImEiOiJjazZ5bmtpeGkwanVtM2xteXJydzF1MndsIn0.SUAHhJFceT7AQ1fb2cIoLw';
-
