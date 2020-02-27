90c8947c39cbe9b66c17dd15a08917a8879c91c1
jessica kim <J_Kim@users.noreply.github.com>
2020-02-27 14:58:41 -0600

    color added

--- static/js/map.js
+++ static/js/map.js
@@ -1,21 +1,4 @@
 
-// d3.json('/API/Data/Map').then(function(response){
-      
-//       var data = response;
-//       console.log(data)
-
-//       var layout = {
-//           title: 'Test',
-//           geo:{
-//               scope: 'usa',
-//               showlakes: true,
-//               lakecolor: 'rgb(255,255,255)'
-//           }
-//       };
-
-//       Plotly.newPlot("map", data, layout, {showLink: false});
-// });
-
 var url = "/API/Data/MapData"
 
 var state_abbr = {'Alaska': 'AK', 'Alabama': 'AL', 'Arkansas': 'AR', 'American Samoa': 'AS', 'Arizona': 'AZ', 'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 'District of Columbia': 'DC', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA', 'Guam': 'GU', 'Hawaii': 'HI', 'Iowa': 'IA', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Massachusetts': 'MA', 'Maryland': 'MD', 'Maine': 'ME', 'Michigan': 'MI', 'Minnesota': 'MN', 'Missouri': 'MO', 'Northern Mariana Islands': 'MP', 'Mississippi': 'MS', 'Montana': 'MT', 'National': 'NA', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Nebraska': 'NE', 'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'Nevada': 'NV', 'New York': 'NY', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR', 'Pennsylvania': 'PA', 'Puerto Rico': 'PR', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Virginia': 'VA', 'Virgin Islands': 'VI', 'Vermont': 'VT', 'Washington': 'WA', 'Wisconsin': 'WI', 'West Virginia': 'WV', 'Wyoming': 'WY'}
@@ -29,6 +12,7 @@ function buildMap(filterYear){
     var filteredData = data[0][filterYear];
     
     var ranks = filteredData.map(d => d['All_Determinants_Rank']);
+
     var state_codes = filteredData.map(d => state_abbr[d['State']]);
     var states = filteredData.map(d => d['State']);
 
@@ -38,19 +22,24 @@ function buildMap(filterYear){
       locations: state_codes,
       z: ranks,
       text: states,
-      autocolorscale: true
+      colorscale: [
+        [0, 'rgb(194,219,194)'], [0.2, 'rgb(123,188,176)'],
+        [0.4, 'rgb(85,156,158)'], [0.6, 'rgb(58,124,137)'],
+        [0.8, 'rgb(35,93,114)'], [1, 'rgb(18,63,90)']
+    ],
+      
   }];
   
   var layout = {
-  title: '2014 US Popultaion by State',
+  title: 'Health Ranking Changes by State',
       geo:{
           scope: 'usa',
-          countrycolor: 'rgb(255, 255, 255)',
+          countrycolor: 'rgb(252,141,89)',
           showland: true,
-          landcolor: 'rgb(217, 217, 217)',
+          landcolor: 'rgb(255,255,191)',
           showlakes: true,
           lakecolor: 'rgb(255, 255, 255)',
-          subunitcolor: 'rgb(255, 255, 255)',
+          subunitcolor: 'rgb(145,191,219)',
           lonaxis: {},
           lataxis: {}
       }
@@ -64,101 +53,27 @@ function buildMap(filterYear){
 
 
 buildMap(2019);
-// Creating map object
-
-// var myMap = L.map('map', {
-//     center: [39.8283, - 98.5795],
-//     zoom: 8 
-// })
-
-// // Tile Layer
-// L.tileLayer(
-//     'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-//   {
-//     maxZoom: 18,
-//     id: 'mapbox.streets',
-//     accessToken: API_KEY
-//   }
-// ).addTo(myMap);
-
-// var acaJson;
-
-// Load in Data
-// d3.json(url).then(function(grabData) {
-//     var states = grabData.map(value => value.States);
-//     var years = grabData.map(value => value.Year);
-//     var thisYear = 2019;
-//     var yearData;
-    
-//     // var stateData = state[0]["Data"];
-//     // console.log(stateData['All_Determinants_Rank']);
-//     // console.log(stateData['All_Determinants_Rank'])
-//     // Create choropleth layer
-//     acaJson = L.choropleth(grabData, {
-//         yearData: grabData.filter(value => value['Year'] == '2019'),
-//         stateData: yearData.map(value => value['State'][0]['Data']),
-       
-//         valueProperty: yearData.map(value => value.All_Determinants_Rank),       
-        
-//         // Set color scale
-//         scale: ['#ffffb2', '#b10026'],
-
-//         // Number of breaks in step range: Ammount of color saturation in gradient to move from one scale to another.
-//         steps: 5,
-
-//         // How am I defining my steps? Quartile - Health rating 
-//         mode: 'q',
-//         style: {
-//             color: '#fff',
-//             weight: 1,
-//             fillOpacity: 0.8
-//         },
 
 
-        
-    //     // Pop for Additional Data
-    //     onEachFeature: function (feature, layer) {
-    //         layer.bindPopup(
-    //           'State: ' +
-    //           feature.index.State +
-    //           '<br>Total Enrollment:<br>' + +
-    //           feature.index.Total_Enrollment
-    //         );
-    //       }
-    //     }).addTo(myMap);
-    //     console.log(stateData),
-    //     console.log(data.rank);
-    //     // Set up the legend
-    //     var legend = L.control({ position: 'bottomright' });
-    //     legend.onAdd = function () {
-    //       var div = L.DomUtil.create('div', 'info legend');
-    //       var limits = acaData.options.limits;
-    //       var colors = acaData.options.colors;
-    //       var labels = [];
+d3.json(url).then(function(data) {
+  //Extract the states and use these to create a listing for the state dropdown menu.
+  var states = data.map(value => value.State);
+  //console.log(states);
 
-    //       // Add min & max
-    //       var legendInfo =
-    //       '<h1>Health Rating</h1>' +
-    //       '<div class="labels">' +
-    //       '<div class="min">' +
-    //       limits[0] +
-    //       '</div>' +
-    //       '<div class="max">' +
-    //       limits[limits.length - 1] +
-    //       '</div>' +
-    //       '</div>';
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("#selDataset1");
 
-    //     div.innerHTML = legendInfo;
+  //Initial menu option should be "State"
+  var cell = dropdownMenu.append("option");
+  cell.text("State");
 
-    //     limits.forEach(function (limit, index) {
-    //       labels.push('<li style="background-color: ' + colors[index] + '"></li>');
-    //     });
 
-    //     div.innerHTML += '<ul>' + labels.join('') + '</ul>';
-    //     return div;
-    //   };
+  //Loop through states for appending to dropdownMenu below "State"
+  states.forEach(function(newState) {
 
-    // // Adding legend to the map
-    // legend.addTo(myMap);
+      //For each new sampleNumber, append a new row and state text.
+      var cell = dropdownMenu.append("option");
+      cell.text(newState);
 
-    // });
+      });
+});
