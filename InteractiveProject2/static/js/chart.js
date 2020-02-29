cfc84c3be9a7267fe854752eae36f6c0b72084dd
Raquel McLellan <raquelmclellan@gmail.com>
2020-02-28 22:37:30 -0600

    midstep updates style.css and chart.js

--- InteractiveProject2/static/js/chart.js
+++ InteractiveProject2/static/js/chart.js
@@ -1,361 +1,101 @@
-//FIRST MENU FOR COMPARISON DATA
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 100, bottom: 30, left: 30},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-//Start building app.js file for extracting ACA data for Menu 1
-var url = "/API/Data"
+// append the svg object to the body of the page
+var svg = d3.select("#my_chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-//DROPDOWN MENU POPULATING WITH STATE NAMES FROM DATA
-//Extract samples data from json file.
-d3.json(url).then(function(data) {
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv", function(data) {
 
-    //console.log(data);
+    // List of groups (here I have one group per column)
+    var allGroup = ["valueA", "valueB", "valueC"]
 
-    //Extract the states and use these to create a listing for the state dropdown menu.
-    var states = data.map(value => value.State);
-    //console.log(states);
+    // add the options to the button
+    d3.select("#selectButton")
+      .selectAll('myOptions')
+     	.data(allGroup)
+      .enter()
+    	.append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset1");
+    // Add X axis --> it is a date format
+    var x = d3.scaleLinear()
+      .domain([0,10])
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-    //Initial menu option should be "State"
-    var cell = dropdownMenu.append("option");
-    cell.text("State");
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain( [0,20])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-    //Loop through states for appending to dropdownMenu below "State"
-    states.forEach(function(newState) {
+    // Initialize line with group a
+    var line = svg
+      .append('g')
+      .append("path")
+        .datum(data)
+        .attr("d", d3.line()
+          .x(function(d) { return x(+d.time) })
+          .y(function(d) { return y(+d.valueA) })
+        )
+        .attr("stroke", "black")
+        .style("stroke-width", 4)
+        .style("fill", "none")
 
-        //For each new sampleNumber, append a new row and state text.
-        var cell = dropdownMenu.append("option");
-        cell.text(newState);
+    // Initialize dots with group a
+    var dot = svg
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+        .attr("cx", function(d) { return x(+d.time) })
+        .attr("cy", function(d) { return y(+d.valueA) })
+        .attr("r", 7)
+        .style("fill", "#69b3a2")
 
-        });
 
-    //DROPDOWN MENU FOR POPULATING WITH YEAR DATA
-    //Extract the years and use to create a listing for the year dropdown menu
-    var year = data.map(value => value.Year);
-    
-    //Use D3 to select the dropdown menu
-    var dropdownMenuYear = d3.select("#selDatayear1");
+    // A function that update the chart
+    function update(selectedGroup) {
 
-    //Initial option should be "Year"
-    var cell = dropdownMenuYear.append("option");
-    cell.text("Year");
+      // Create new data with the selection?
+      var dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
 
-    //If I just looped as above, there will be 52 lines for every year in the data.
-    //Create an array, then append to that array, then remove duplicates!
-    yearArray = [];
+      // Give these new data to update line
+      line
+          .datum(dataFilter)
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()
+            .x(function(d) { return x(+d.time) })
+            .y(function(d) { return y(+d.value) })
+          )
+      dot
+        .data(dataFilter)
+        .transition()
+        .duration(1000)
+          .attr("cx", function(d) { return x(+d.time) })
+          .attr("cy", function(d) { return y(+d.value) })
+    }
 
-    year.forEach(function(newYear){
-        yearArray.push(newYear);
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton").on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
     })
-    //console.log(yearArray);
 
-    uniqueYear = new Set(yearArray);
-    //console.log(uniqueYear);
-
-    //Loop through years and append to dropdown menu for below "Year"
-    uniqueYear.forEach(function(uniqueYear) {
-        //For each newYear, append a new row and year text
-        var cell = dropdownMenuYear.append("option");
-        cell.text(uniqueYear);
-    });
-
-    });
-
-
-//EVENT HANDLER FOR DROPDOWN MENU UPDATE PLOTLY EVENT: INTERACTIVE PLOTS
-d3.select("#selDataset1").on("change", updateTable1);
-
-//updateTABLE FUNCTION
-function updateTable1() {
-
-    //Retrieve data from url /API/Data
-    var url = "/API/Data"
-
-    d3.json(url).then(function(data) {
-
-    //console.log(data);
-
-    //DROPDOWN MENU SECTION
-        //Select the #selDataset again to now use with all of the state names available.
-        var sampleMenu = d3.select("#selDataset1");
-
-        //Select the #selDatayear1 to use the year.
-        var sampleYear = d3.select("#selDatayear1");
-
-        //Assign the value of the dropdown state menu option to pickedState variable for data filtering.
-        var pickedState = sampleMenu.property("value");
-        console.log(pickedState);
-
-        //Assign the value of the dropdown year menu option to pickedYear variable for data filtering.
-        var pickedYear = sampleYear.property("value");
-        console.log(pickedYear);
-
-    //FILTERING BY MENU SELECTION
-        //To filter the data to entries where the "State" is the desired state from pulldown menu.
-        //Note that the == and not ===, the Year is a string search, but the array value is a number!
-        var state = data.filter(value => value["State"] == pickedState && value["Year"] == pickedYear);
-        // console.log(data);
-        // console.log(state);
-
-        //To extract the state "Data" array from the state whose data we wanted from pulldown menu
-        var statedata = state[0]["Data"];
-        //console.log(statedata);
-
-        //Extract some data from the "Data" array
-        var statename = statedata["State"]
-        var year = statedata["Year"];
-        var cancerdeaths = statedata["Cancer_Deaths_Value"];
-        var cardiodeaths = statedata["Cardiovascular_Deaths_Value"];
-        var cholesterolcheck = statedata["Cholesterol_Check_Value"];
-        var diabetes = statedata["Diabetes_Value"];
-        var disparity = statedata["Disparity_in_Health_Status_Value"];
-        var drugdeaths = statedata["Drug_Deaths_Value"];
-        var bloodpressure = statedata["High_Blood_Pressure_Value"];
-        var infantmort = statedata["Infant_Mortality_Value"];
-        var immunized = statedata["Immunizations_Children_Value"];
-        var premature = statedata["Premature_Death_Value"];
-        var primaryphys = statedata["Primary_Care_Physicians_Value"];
-        var uninsured = statedata["Uninsured_Value"];
-
-        
-        // console.log(data);
-        // console.log(state);
-        // console.log(statedata);
-        // console.log(cardiodeaths);
-        // console.log(diabetes);
-        // console.log(infantmort);
-
-        //Clear previous data in demographicMenu: Select in Line 1, Reassign to nothing in Line 2
-        var oldStateMenu = d3.select("#sample-metadata1");
-        oldStateMenu.html("");   
-            
-        //Select sample-metadata id using d3. This is where I will insert text for demographicData
-        var StateMenu = d3.select("#sample-metadata1");
-
-            var cell = StateMenu.append("h4");
-            cell.text(`State: ${statename}`);
-
-            var cell = StateMenu.append("h4");
-            cell.text(`Year: ${year}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Cancer Deaths: ${cancerdeaths}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Cardiovascular Deaths: ${cardiodeaths}`);
-            
-            var cell = StateMenu.append("p");
-            cell.text(`Cholesterol Check: ${cholesterolcheck}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Diabetes: ${diabetes}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Disparity in Health Status: ${disparity}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Drug Deaths: ${drugdeaths}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`High Blood Pressure: ${bloodpressure}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Infant Mortality: ${infantmort}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Immunizations-Children: ${immunized}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Premature Deaths: ${premature}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Primary Care Physicians: ${primaryphys}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Uninsured: ${uninsured}`);
-                
-});
-
-};
-
-//SECOND MENU FOR COMPARISON DATA
-
-//Start building app.js file for extracting ACA data for Menu 2
-var url = "/API/Data"
-
-//DROPDOWN MENU POPULATING WITH STATE NAMES FROM DATA
-//Extract samples data from json file.
-d3.json(url).then(function(data) {
-
-    //console.log(data);
-
-    //Extract the ID numbers and use these to create a listing for the dropdown menu.
-    var states = data.map(value => value.State);
-    //console.log(states);
-
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset2");
-
-    //Initial menu option should be "ID Number"
-    var cell = dropdownMenu.append("option");
-    cell.text("State");
-
-    //Loop through states for appending to dropdownMenu below "State"
-    states.forEach(function(newSample) {
-
-        //For each new sampleNumber, append a new row and text.
-        var cell = dropdownMenu.append("option");
-        cell.text(newSample);
-
-        });
-    
-    //DROPDOWN MENU FOR POPULATING WITH YEAR DATA
-    //Extract the years and use to create a listing for the year dropdown menu
-    var year = data.map(value => value.Year);
-    
-    //Use D3 to select the dropdown menu
-    var dropdownMenuYear = d3.select("#selDatayear2");
-
-    //Initial option should be "Year"
-    var cell = dropdownMenuYear.append("option");
-    cell.text("Year");
-
-    //If I just looped as above, there will be 52 lines for every year in the data.
-    //Create an array, then append to that array, then remove duplicates!
-    yearArray = [];
-
-    year.forEach(function(newYear){
-        yearArray.push(newYear);
-    })
-    //console.log(yearArray);
-
-    uniqueYear = new Set(yearArray);
-    //console.log(uniqueYear);
-
-    //Loop through years and append to dropdown menu for below "Year"
-    uniqueYear.forEach(function(uniqueYear) {
-        //For each newYear, append a new row and year text
-        var cell = dropdownMenuYear.append("option");
-        cell.text(uniqueYear);
-    });
-
-    });
-
-
-//EVENT HANDLER FOR DROPDOWN MENU UPDATE PLOTLY EVENT: INTERACTIVE PLOTS
-d3.select("#selDataset2").on("change", updateTable2);
-
-//updateTABLE FUNCTION
-function updateTable2() {
-
-    //Retrieve data from url /API/Data
-    var url = "/API/Data"
-
-    d3.json(url).then(function(data) {
-
-    console.log(data);
-
-    //DROPDOWN MENU SECTION
-        //Select the #selDataset again to now use with all of the sampleNumbers available.
-        var sampleMenu = d3.select("#selDataset2");
-
-        //Select the #selDatayear2 to use the year.
-        var sampleYear = d3.select("#selDatayear2");
-
-        //Assign the value of the dropdown menu option to sampleID variable for data filtering.
-        var pickedState = sampleMenu.property("value");
-        console.log(pickedState);
-
-        //Assign the value of the dropdown year menu option to pickedYear variable for data filtering.
-        var pickedYear = sampleYear.property("value");
-        console.log(pickedYear);
-
-    //FILTERING THE DATA BY STATE AND YEAR
-        //To filter the data to entries where the "State" is the desired state from pulldown menu.
-        //Note that the == and not ===, the Year is a string search, but the array value is a number!
-        var state = data.filter(value => value["State"] == pickedState && value["Year"] == pickedYear);
-        // console.log(data);
-        // console.log(state);
-
-        //To extract the state "Data" array from the state whose data we wanted from pulldown menu
-        var statedata = state[0]["Data"];
-        console.log(statedata);
-
-        //Extract some data from the "Data" array
-        var statename = statedata["State"]
-        var year = statedata["Year"];
-        var cancerdeaths = statedata["Cancer_Deaths_Value"];
-        var cardiodeaths = statedata["Cardiovascular_Deaths_Value"];
-        var cholesterolcheck = statedata["Cholesterol_Check_Value"];
-        var diabetes = statedata["Diabetes_Value"];
-        var disparity = statedata["Disparity_in_Health_Status_Value"];
-        var drugdeaths = statedata["Drug_Deaths_Value"];
-        var bloodpressure = statedata["High_Blood_Pressure_Value"];
-        var infantmort = statedata["Infant_Mortality_Value"];
-        var immunized = statedata["Immunizations_Children_Value"];
-        var premature = statedata["Premature_Death_Value"];
-        var primaryphys = statedata["Primary_Care_Physicians_Value"];
-        var uninsured = statedata["Uninsured_Value"];
-
-        // console.log(data);
-        // console.log(state);
-        // console.log(statedata);
-        // console.log(cardiodeaths);
-        // console.log(diabetes);
-        // console.log(infantmort);
-
-
-
-
-        //Clear previous data in demographicMenu: Select in Line 1, Reassign to nothing in Line 2
-        var oldStateMenu = d3.select("#sample-metadata2");
-        oldStateMenu.html("");   
-            
-        //Select sample-metadata id using d3. This is where I will insert text for demographicData
-        var StateMenu = d3.select("#sample-metadata2");
-
-            var cell = StateMenu.append("h4");
-            cell.text(`State: ${statename}`);
-
-            var cell = StateMenu.append("h4");
-            cell.text(`Year: ${year}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Cancer Deaths: ${cancerdeaths}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Cardiovascular Deaths: ${cardiodeaths}`);
-            
-            var cell = StateMenu.append("p");
-            cell.text(`Cholesterol Check: ${cholesterolcheck}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Diabetes: ${diabetes}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Disparity in Health Status: ${disparity}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Drug Deaths: ${drugdeaths}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`High Blood Pressure: ${bloodpressure}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Infant Mortality: ${infantmort}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Immunizations-Children: ${immunized}`);
- 
-            var cell = StateMenu.append("p");
-            cell.text(`Premature Deaths: ${premature}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Primary Care Physicians: ${primaryphys}`);
-
-            var cell = StateMenu.append("p");
-            cell.text(`Uninsured: ${uninsured}`);
-                
-});
-
-};
+})
